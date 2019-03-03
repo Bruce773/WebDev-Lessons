29a67acebbfde0e12bc091587b991a69ceaea4a3
Bruce <bruce@yogainternational.com>
2019-03-02 22:33:32 -0500

    removes unneed files

--- components/LessonList.jsx
+++
@@ -1,5 +0,0 @@
-import Lesson from 'Lesson.jsx'
-
-var LessonList = (props) => {
-  
-}
--- fourOFourErrorHTML.js
+++
@@ -1,5 +0,0 @@
-var fourOhFourErroHTML = `
-<h1>404</h1>
-<h2>The page you're looking for doesn't exist!</h2>
-<h3>Please ensure you've typed the URL properly... capitalization <b>does</b> matter.</h3>
-`
