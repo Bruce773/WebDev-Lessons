0e2a74f3dd3bb0fb28719cc533cc28eeeacf3f04
Bruce <bruce@yogainternational.com>
2019-05-28 21:03:15 -0400

    adds lines of code to test get request

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/../index.html'));
 
+app.get('/', (req, res) => {
+  res.send('Testing... homepage');
+});
+
 app.listen(process.env.PORT || 4000, () => {
   console.log(
     `Server running on port ${process.env.PORT ? process.env.PORT : 4000}`
