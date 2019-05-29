7fc87d99992919de0cb60435a7ebb613ddcdee31
Bruce <bruce@yogainternational.com>
2019-05-28 21:08:38 -0400

    removes uneeded code  Please enter the commit message for your changes. Lines starting

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/../index.html'));
 
-app.get('/', (req, res) => {
-  res.send(express.static(__dirname + '/../index.html'));
-});
-
 app.listen(process.env.PORT || 4000, () => {
   console.log(
     `Server running on port ${process.env.PORT ? process.env.PORT : 4000}`
