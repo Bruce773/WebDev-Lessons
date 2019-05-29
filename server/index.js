386510d93e1fe0b9a05b76603944e3b2e08d53dd
Bruce <bruce@yogainternational.com>
2019-05-28 20:50:27 -0400

    fixes express dirname errors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+app.use(express.static(__dirname + '/../index.html'));
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(
