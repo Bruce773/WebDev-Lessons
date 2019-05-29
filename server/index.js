ad1175df3d6b8a238ef8f6b62f01f09881dc9796
Bruce <bruce@yogainternational.com>
2019-05-28 20:37:25 -0400

    add Express server and fixes start script to run it

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('./node_modules/express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(
+    `Server running on port ${process.env.PORT ? process.env.PORT : 4000}`
+  );
+});
